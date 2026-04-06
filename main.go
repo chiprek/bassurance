dec7ade0caa33c2cc1e470ab7f0b3e7f06119726
chiprek <chiprek.cs@gmail.com>
2026-04-06 16:18:56 -0500

    Updated main to have http functionality for experimentation will refactor this later

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Initalizing Benchmark assurance")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Benchmark assurance is running")
+	})
+
+	fmt.Println("Server starting on :8080")
+	http.ListenAndServe(":8080", nil)
+
 }
