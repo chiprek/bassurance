8c174dca624f85f1c9ebdfc714258d3e0c840322
chiprek <chiprek.cs@gmail.com>
2026-04-11 11:02:39 -0500

    updated db.go

--- db.go
+++ db.go
@@ -1,11 +1,11 @@
 //so i have a predicament here is one of my sql table and have forgotten how to do multi line strings
 // "CREATE TABLE IF NOT EXISTS step (
-//       id INTEGER PRIMARY KEY, 
-//       process_id INTEGER NOT NULL, 
-//       name TEXT NOT NULL, 
-//       description TEXT NOT NULL, 
-//       required INTEGER NOT NULL, 
-//       critical INTEGER NOT NULL, 
+//       id INTEGER PRIMARY KEY,
+//       process_id INTEGER NOT NULL,
+//       name TEXT NOT NULL,
+//       description TEXT NOT NULL,
+//       required INTEGER NOT NULL,
+//       critical INTEGER NOT NULL,
 //       step_order INTEGER NOT NULL,
 //       FOREIGN KEY(process_id) REFERENCES process(id));"
 
@@ -17,15 +17,28 @@ import (
 
 func initDB() (*sql.DB, error) {
 	DB, err := sql.Open("sqlite", "bassurance.sqlite")
-	sql := `CREATE TABLE IF NOT EXISTS step (
-        id INTEGER PRIMARY KEY, 
-        process_id INTEGER NOT NULL, 
-        name TEXT NOT NULL, 
-        description TEXT NOT NULL, 
-        required INTEGER NOT NULL, 
-        critical INTEGER NOT NULL, 
+	sql := []string{
+		`create table users (
+  		id INT not null,
+   		name TEXT not null,
+    	description TEXT not null,
+  		created_at TEXT not null,
+  		created_by TEXT not null,
+  		primary key (id))`,
+
+		`CREATE TABLE IF NOT EXISTS step (
+        id INTEGER PRIMARY KEY,
+        process_id INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        description TEXT NOT NULL,
+        required INTEGER NOT NULL,
+        critical INTEGER NOT NULL,
         step_order INTEGER NOT NULL,
-        FOREIGN KEY(process_id) REFERENCES process(id));`
-        
-    DB.Exec(sql)
+        FOREIGN KEY(process_id) REFERENCES process(id));`,
+	}
+	if err != nil {
+
+	}
+
+	return DB, nil
 }
