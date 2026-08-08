6399b2701a1927074d5dad33fbad13794a578a68
chiprek <chiprek.cs@gmail.com>
2026-08-08 16:29:19 -0500

    Update 004_create_sub_assemblies.sql

--- sql/schema/004_create_sub_assemblies.sql
+++ sql/schema/004_create_sub_assemblies.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 CREATE TABLE sub_assemblies (
     id UUID PRIMARY KEY,
-    unit_id UUID REFERENCES units(id),
+    unit_id UUID REFERENCES units(id) NOT NULL,
     name TEXT NOT NULL,
     serial_number TEXT,
     status TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMPTZ
 );
 
