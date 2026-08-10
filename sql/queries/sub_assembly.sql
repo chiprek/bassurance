6b4efe8b5e90922a718e0e43c233d7688f78c7c1
chiprek <chiprek.cs@gmail.com>
2026-08-09 20:59:08 -0500

    Update sub_assembly.sql

--- sql/queries/sub_assembly.sql
+++ sql/queries/sub_assembly.sql
@@ -4,3 +4,7 @@ id, unit_id, name, serial_number, status
 )
 VALUES ( $1, $2, $3, $4, $5)
 RETURNING *;
+
+-- name: GetSubAssembly :one
+SELECT * FROM sub_assemblies
+WHERE unit_id = $1 AND deleted_at IS NULL;
