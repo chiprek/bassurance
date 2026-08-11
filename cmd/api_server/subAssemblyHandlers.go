adc082dcd75d0c29cd6763f7c37a93caab1fb34a
chiprek <chiprek.cs@gmail.com>
2026-08-10 19:00:43 -0500

    added endpoint to get all sub assemblies of a given unit

--- cmd/api_server/subAssemblyHandlers.go
+++ cmd/api_server/subAssemblyHandlers.go
@@ -3,7 +3,9 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"net/http"
+	"sort"
 
 	"github.com/chiprek/bassurance/internal/database"
 	"github.com/google/uuid"
@@ -49,4 +51,62 @@ func (cfg *apiConfig) handleCreateSubAssembly(w http.ResponseWriter, r *http.Req
 	}
 
 	respondWithJSON(w, http.StatusOK, subAssembly)
+}
+
+func (cfg *apiConfig) handleGetSubAssemblies(w http.ResponseWriter, r *http.Request) {
+	sortDirection := r.URL.Query().Get("sort")
+
+	type SubAssembly struct {
+		ID           uuid.UUID
+		Name         string
+		SerialNumber sql.NullString
+		Status       string
+		CreatedAt    sql.NullTime
+	}
+
+	unitSN := r.PathValue("serial_number")
+
+	dbUnit, err := cfg.Queries.GetUnitBySerialNumber(r.Context(), unitSN)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, "unit not found")
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, "unable to fetch unit")
+		return
+	}
+	var uSubAsm []database.SubAssembly
+	uSubAsm, err = cfg.Queries.GetSubAssemblies(r.Context(), dbUnit)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "something went wrong")
+		return
+	}
+
+	allSubs := make([]SubAssembly, 0, len(uSubAsm))
+	for _, allSub := range allSubs {
+		allSubs = append(allSubs, SubAssembly{
+			ID:           allSub.ID,
+			Name:         allSub.Name,
+			Status:       allSub.Status,
+			SerialNumber: allSub.SerialNumber,
+			CreatedAt:    allSub.CreatedAt,
+		})
+	}
+
+	switch sortDirection {
+	case "desc":
+		sort.Slice(allSubs, func(i, j int) bool {
+			return allSubs[i].CreatedAt.Time.After(allSubs[j].CreatedAt.Time)
+		})
+	case "asc", "":
+		fallthrough
+	default:
+		sort.Slice(allSubs, func(i, j int) bool {
+			return allSubs[i].CreatedAt.Time.Before(allSubs[j].CreatedAt.Time)
+		})
+	}
+
+	respondWithJSON(w, http.StatusOK, allSubs)
+
 }
--- sql/queries/sub_assembly.sql
+++ sql/queries/sub_assembly.sql
@@ -5,6 +5,6 @@ id, unit_id, name, serial_number, status
 VALUES ( $1, $2, $3, $4, $5)
 RETURNING *;
 
--- name: GetSubAssembly :one
+-- name: GetSubAssemblies :many
 SELECT * FROM sub_assemblies
 WHERE unit_id = $1 AND deleted_at IS NULL;
