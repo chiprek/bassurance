0f3f243cea311ecb79efe43527a63cacbd44c968
chiprek <chiprek.cs@gmail.com>
2026-09-01 20:51:39 -0500

    Update 005_create_assembly_logs.sql

--- sql/schema/005_create_assembly_logs.sql
+++ sql/schema/005_create_assembly_logs.sql
@@ -1,4 +1,9 @@
 -- +goose up
 CREATE TABLE assembly_logs(
     id UUID PRIMARY KEY,
+    sub_assembly_id UUID REFERENCES sub_assemblies(id),
+    file_path TEXT NOT NULL,
+    file_hash TEXT NOT NULL,
+    captured_at TIMESTAMPTZ NOT NULL,
+
 )
