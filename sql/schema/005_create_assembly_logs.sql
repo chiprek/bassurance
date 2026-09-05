1a0df5d0780cfcdb3c9fc541c176c775bbba1bd9
chiprek <chiprek.cs@gmail.com>
2026-09-05 17:52:44 -0500

    Update 005_create_assembly_logs.sql

--- sql/schema/005_create_assembly_logs.sql
+++ sql/schema/005_create_assembly_logs.sql
@@ -1,16 +1,16 @@
 -- +goose up
 CREATE TABLE assembly_logs(
     id UUID PRIMARY KEY,
-    sub_assembly_id UUID REFERENCES sub_assemblies(id),
+    sub_assembly_id UUID NOT NULL REFERENCES sub_assemblies(id),
     file_path TEXT NOT NULL,
     file_hash TEXT NOT NULL,
     captured_at TIMESTAMPTZ NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    deleted_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMPTZ
 
 );
 
-CREATE INDEX idx_assembly_logs_if ON assembly_logs(sub_assembly_id, file_hash);
+CREATE INDEX idx_assembly_logs_dedup ON assembly_logs(sub_assembly_id, file_hash);
 
 -- +goose down
 DROP TABLE IF EXISTS assembly_logs;
