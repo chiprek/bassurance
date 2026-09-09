da487ab6b142761561b49d9c7c7302dca316025a
chiprek <chiprek.cs@gmail.com>
2026-09-08 19:15:04 -0500

    Update assembly_logs.sql

--- sql/queries/assembly_logs.sql
+++ sql/queries/assembly_logs.sql
@@ -6,3 +6,28 @@ SELECT EXISTS (
       AND file_hash = $2
       AND deleted_at IS NULL
 );
+
+-- name: CreateAssemblyLog :one
+INSERT INTO assembly_logs (
+    id,
+    sub_assembly_id,
+    file_path,
+    file_hash,
+    captured_at
+) VALUES (
+    $1, $2, $3, $4, $5
+)
+RETURNING *;
+
+-- name: GetPhotosBySubAssembly :many
+SELECT *
+FROM assembly_logs
+WHERE sub_assembly_id = $1
+    AND deleted_at IS NULL
+ORDER BY captured_at DESC
+LIMIT $2 OFFSET $3;
+
+-- name: DeleteAssemblyLog :exec
+UPDATE assembly_logs
+SET deleted_at = CURRENT_TIMESTAMP
+WHERE id = $1 AND deleted_at IS NULL;
