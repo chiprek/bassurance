5c0433fbec2927bbbb99f44fce722b2a3b8a5007
chiprek <chiprek.cs@gmail.com>
2026-09-10 22:16:29 -0500

    Update main.go

--- cmd/api_server/main.go
+++ cmd/api_server/main.go
@@ -49,7 +49,7 @@ func main() {
 	mux.HandleFunc("GET /api/v1/units", cfg.handlerListUnits)
 	mux.HandleFunc("POST /api/v1/units/{serial_number}/sub-assemblies", cfg.handleCreateSubAssembly)
 	mux.HandleFunc("GET /api/v1/units/{serial_number}/sub-assemblies", cfg.handleGetSubAssemblies)
-	mux.HandleFunc("POST /api/v1/jobs/{name}/photos")
+	mux.HandleFunc("POST /api/v1/jobs/{name}/photos", cfg.HandleCreateJobNote)
 
 	server := &http.Server{
 		Addr:    ":" + port,
