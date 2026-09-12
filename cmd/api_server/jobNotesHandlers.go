eed1d9b640a1f36958459eb93e4cbe315b22e36f
chiprek <chiprek.cs@gmail.com>
2026-09-11 21:19:00 -0500

    Update jobNotesHandlers.go

--- cmd/api_server/jobNotesHandlers.go
+++ cmd/api_server/jobNotesHandlers.go
@@ -3,5 +3,9 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) HandleCreateJobNote(w http.ResponseWriter, r *http.Request) {
+	UrlName := r.PathValue("name")
+
+	sanitized := normalize(UrlName)
+
 	respondWithJSON()
 }
