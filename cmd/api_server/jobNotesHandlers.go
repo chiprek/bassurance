94f99fa074a55878cc5941d4cb70a69770655856
chiprek <chiprek.cs@gmail.com>
2026-09-21 19:54:04 -0500

    Update jobNotesHandlers.go

--- cmd/api_server/jobNotesHandlers.go
+++ cmd/api_server/jobNotesHandlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -17,7 +18,20 @@ func (cfg *apiConfig) HandleCreateJobNote(w http.ResponseWriter, r *http.Request
 		respondWithError(w, http.StatusBadRequest, "failed to get photo")
 		return
 	}
+	defer file.Close()
+
+	var jobName string
+	var subAssembly string
+
+	distinctDir := filepath.Join("./uploads", jobName, subAssembly)
+
 	filename := filepath.Base(header.Filename)
-	fullPath := filepath.Join()
+	fullPath := filepath.Join(distinctDir, filename)
+
+	dst, err := os.Create(fullPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to save file")
+		return
+	}
 
 }
